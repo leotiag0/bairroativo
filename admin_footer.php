a41509f961d9e97d697d3c21ba7e425b1280c09d
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-10 12:05:31 -0300

    Create admin_footer.php

---
+++ admin_footer.php
@@ -0,0 +1,4 @@
+<!-- admin_footer.php -->
+<footer style="background:#007bff; color:white; text-align:center; padding:15px; margin-top:60px;">
+    &copy; <?= date('Y') ?> Sistema Bairro Ativo. Todos os direitos reservados.
+</footer>
