e94a5d02960e80e48307a0de8785a8dada8fe189
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-10 15:41:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,12 @@
     <h2><?= $t['titulo'] ?></h2>
     <p><?= $t['bem_vindo'] ?> ao sistema!</p>
     <img src="images/como_funciona.jpg" style="max-width:100%; margin:20px 0;">
+    <div style="margin-top: 20px;">
     <a href="mapa.php?lang=<?= $lang ?>" class="btn">🗺️ Ver Mapa</a>
+    <a href="lista.php?lang=<?= $lang ?>" class="btn">📋 Ver Lista</a>
     <a href="admin_login.php" class="btn">🔐 Admin</a>
+</div>
+
 </div>
 
 <footer>
