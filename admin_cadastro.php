8372deff3bdb89104de07d4a5f3c96ec8344ce1c
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-10 10:54:41 -0300

    Update admin_cadastro.php

--- admin_cadastro.php
+++ admin_cadastro.php
@@ -33,6 +33,8 @@ function getCoordinates($endereco, $apiKey, $pdo) {
     return null;
 }
 
+include 'admin_menu.php';
+
 $msg = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome     = $_POST['nome'];
