2e50cee4ceef8ecc66c3fccfed59ce4b87f6ffb8
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-10 10:41:05 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db   = 'sua_base';
-$user = 'seu_usuario';
-$pass = 'sua_senha';
+$host = 'srv881.hstgr.io';
+$db   = 'u918328567_bairroativo';
+$user = 'u918328567_ba1';
+$pass = 'g/i7Div.&F8/njX';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
