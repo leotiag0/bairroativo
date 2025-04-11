c9b196cdeef82b524f0b8b6173f5a2227366b314
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-11 12:54:00 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
 <footer>
     &copy; <?= date('Y') ?> Sistema Bairro Ativo. Todos os direitos reservados.
+        <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const btnContraste = document.getElementById("toggle-contraste");
+    
+        if (localStorage.getItem('contraste') === '1') {
+            document.body.classList.add('contraste-alto');
+        }
+    
+        btnContraste?.addEventListener("click", function () {
+            document.body.classList.toggle('contraste-alto');
+            const ativo = document.body.classList.contains('contraste-alto') ? '1' : '0';
+            localStorage.setItem('contraste', ativo);
+        });
+    });
+    </script>
 </footer>
