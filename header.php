509903903e58dd9ce39832b01d7d43e4ff8e1369
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-11 12:19:34 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php if (!isset($lang)) include 'lang.php'; ?>
+<header>
+    <a href="index.php"><img src="images/logo.png" alt="Logo" class="logo"></a>
+    <nav class="menu">
+        <a href="?lang=pt"><img src="images/brasil-flag.jpg" alt="Português" class="flag-icon"></a>
+        <a href="?lang=es"><img src="images/spain-flag.jpg" alt="Español" class="flag-icon"></a>
+        <a href="?lang=en"><img src="images/uk-flag.jpg" alt="English" class="flag-icon"></a>
+    </nav>
+</header>
