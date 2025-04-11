fa08bce3c9907b6ca98e05e73ceccf341b273fcc
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-11 12:42:57 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
-<?php if (!isset($lang)) include 'lang.php'; ?>
 <header>
     <a href="index.php"><img src="images/logo.png" alt="Logo" class="logo"></a>
     <nav class="menu">
         <a href="?lang=pt"><img src="images/brasil-flag.jpg" alt="Português" class="flag-icon"></a>
         <a href="?lang=es"><img src="images/spain-flag.jpg" alt="Español" class="flag-icon"></a>
         <a href="?lang=en"><img src="images/uk-flag.jpg" alt="English" class="flag-icon"></a>
+        <button id="toggle-contrast" class="contrast-toggle" title="Modo de alto contraste">🌓</button>
     </nav>
 </header>
