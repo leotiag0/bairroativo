e2ecc2787957cab4fedd8390d14aa4ee464091f1
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-11 13:54:03 -0300

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <a href="index.php"><img src="images/logo.png" alt="Logo" class="logo"></a>
     </div>
     <div class="header-right">
-        <button id="toggle-contraste" onclick="toggleContraste()">🌙 Contraste</button>
+        <button id="toggle-contraste" aria-label="Ativar/Desativar alto contraste">🌓 Contraste</button>
         <a href="?lang=pt"><img src="images/brasil-flag.jpg" alt="Português" class="flag-icon"></a>
         <a href="?lang=es"><img src="images/spain-flag.jpg" alt="Español" class="flag-icon"></a>
         <a href="?lang=en"><img src="images/uk-flag.jpg" alt="English" class="flag-icon"></a>
