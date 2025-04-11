d046ffd3a27bdef6a6bd61c3ad41c824c28ca3a7
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-11 13:32:37 -0300

    Update header.php

--- header.php
+++ header.php
@@ -7,13 +7,13 @@
 
     <div class="header-right">
         <a href="?lang=pt">
-            <img src="images/brasil-flag.png" alt="Português" class="flag-icon">
+            <img src="images/brasil-flag.jpg" alt="Português" class="flag-icon">
         </a>
         <a href="?lang=es">
-            <img src="images/spain-flag.png" alt="Español" class="flag-icon">
+            <img src="images/spain-flag.jpg" alt="Español" class="flag-icon">
         </a>
         <a href="?lang=en">
-            <img src="images/uk-flag.png" alt="English" class="flag-icon">
+            <img src="images/uk-flag.jpg" alt="English" class="flag-icon">
         </a>
         <button id="toggle-contraste">Contraste</button>
     </div>
