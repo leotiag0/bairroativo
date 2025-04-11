f0a4c5b5652f6feb42cf2989f9336fef2bfa8ac7
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-11 13:37:34 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,20 +1,11 @@
 <header>
     <div class="header-left">
-        <a href="index.php">
-            <img src="images/logo.png" alt="Logo Bairro Ativo" class="logo">
-        </a>
+        <a href="index.php"><img src="images/logo.png" alt="Logo" class="logo"></a>
     </div>
-
     <div class="header-right">
-        <a href="?lang=pt">
-            <img src="images/brasil-flag.jpg" alt="Português" class="flag-icon">
-        </a>
-        <a href="?lang=es">
-            <img src="images/spain-flag.jpg" alt="Español" class="flag-icon">
-        </a>
-        <a href="?lang=en">
-            <img src="images/uk-flag.jpg" alt="English" class="flag-icon">
-        </a>
-        <button id="toggle-contraste">Contraste</button>
+        <button id="toggle-contraste" onclick="toggleContraste()">🌙 Contraste</button>
+        <a href="?lang=pt"><img src="images/brasil-flag.jpg" alt="Português" class="flag-icon"></a>
+        <a href="?lang=es"><img src="images/spain-flag.jpg" alt="Español" class="flag-icon"></a>
+        <a href="?lang=en"><img src="images/uk-flag.jpg" alt="English" class="flag-icon"></a>
     </div>
 </header>
