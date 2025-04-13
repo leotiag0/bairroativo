8c3ba5a4958ea1fef47bf417d291add26ccc099c
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-13 18:06:07 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
-<footer>
-    &copy; <?= date('Y') ?> Sistema Bairro Ativo. Todos os direitos reservados.
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; <?= date('Y') ?> Sistema Bairro Ativo. Todos os direitos reservados.</p>
+    </div>
 </footer>
