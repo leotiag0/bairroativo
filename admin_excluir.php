fd3018458e7ae3404e0a66ba0b176b996c78f1e3
Leonardo Oliveira <leotiago2005@gmail.com>
2025-04-14 13:15:59 -0300

    Update admin_excluir.php

--- admin_excluir.php
+++ admin_excluir.php
@@ -10,7 +10,7 @@ require 'conexao.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['excluir_ids']) && is_array($_POST['excluir_ids'])) {
         $ids = array_map('intval', $_POST['excluir_ids']);
-        if (count($ids)) {
+        if (count($ids) > 0) {
             $placeholders = implode(',', array_fill(0, count($ids), '?'));
             $stmt = $pdo->prepare("DELETE FROM servicos WHERE id IN ($placeholders)");
             $stmt->execute($ids);
